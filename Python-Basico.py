f795a7ff0176e5aed91edb9cf7d359ea9f94cd0b
Jeremias Oliveira <153403079+jere159@users.noreply.github.com>
2025-02-03 00:50:42 +0000

    update

---
+++ Python-Basico.py
@@ -0,0 +1,19 @@
+print ("Olá, Mundo!")
+#Esse é um comentário em Python
+1+1 #Soma
+10-2 #Subtração
+2*2 #Multiplicação
+10/2 #Divisão
+a= 4 #Declaração de variável
+frase = "Python para análise de dados" #Variável String
+frase[0] #Primeiro elemento
+frase[0:10] #Primeiro elemento ao nono
+frase[10:] #Décimo elemento ao último
+frase[:17] #Do início ao décimo sétimo elemento
+frase[::-1] #Elementos em ordem invertida
+frase.count("dados") #Contar elementos
+frase.lower() #elementos em letra minúscula
+frase.upper() #elementos em letra maiúscula
+frase.replace("Python", "Frases") # Substituir elementos
+
+
